75d1753c32081eaa27ce48be939ec3999e1a8fdf
Pablo-Mdz <pablocigoy@gmail.com>
2023-09-26 22:36:19 +0200

    upload img to project

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -6,7 +6,7 @@ import './Projects.css';
 
 const Projects = () => {
   const projectData = [
-    { title: 'Proyecto 1', description: 'Descripción 1', imageUrl: '/images/proyecto1.jpg' },
+    { title: 'Proyecto 1', description: 'Descripción 1', imageUrl: 'https://res-console.cloudinary.com/be-chef/thumbnails/transform/v1/image/upload/Y19saW1pdCxoXzE2MDAsd18xNjAwLGZfanBnLGZsX2xvc3N5LmFueV9mb3JtYXQucHJlc2VydmVfdHJhbnNwYXJlbmN5LnByb2dyZXNzaXZl/v1/ZzFyenhlczgyNG54bmZteHV1b3g=/template_primary' },
     { title: 'Proyecto 2', description: 'Descripción 2', imageUrl: '/images/proyecto2.jpg' },
     { title: 'Proyecto 3', description: 'Descripción 2', imageUrl: '/images/proyecto2.jpg' },
     { title: 'Proyecto 4', description: 'Descripción 2', imageUrl: '/images/proyecto2.jpg' },
