d0acba87ab97da4fbc20e8788871856e7ff8995f
Pablo-Mdz <pablocigoy@gmail.com>
2024-04-09 10:15:42 +0200

    update About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,7 +9,7 @@ const About = () => {
         <div className="about-container">
             <h2 className="about-title">About Me</h2>
             <p className="about-text">
-                I am a software developer. Currently, I am studying mobile app development at the Syntaxt Institute, where I am learning Figma, Kotlin, and Swift.<br></br> In 2022, I completed the full stack web development bootcamp at Ironhack Berlin, and since then, I have been continuously learning and enjoying programming.<br></br>
+                I am a software developer. Currently, I am studying mobile app development at the Syntaxt Institut, where I am learning Figma, Kotlin, and Swift.<br></br> In 2022, I completed the full stack web development bootcamp at Ironhack Berlin, and since then, I have been continuously learning and programming.<br></br>
 
                 Additionally, I work as a programming instructor at the ReDi bootcamp in Berlin. Outside of my professional life, my interests include cooking, traveling, and cycling.<br></br>  I have had the privilege of traveling to 38 countries and have worked in 5 of them, using my skills in teamwork, organizational tasks, task optimization, management, advice, and service in various environments, including private and public events. <br></br>
 
