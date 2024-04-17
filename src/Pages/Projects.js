5ad90ead777936f6642f9ff47ed0496874761f3e
Pablo-Mdz <pablocigoy@gmail.com>
2024-04-17 16:35:20 +0200

    new projects

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -8,12 +8,13 @@ const Projects = () => {
 
     const projectData = [
         { title: 'Andorid App Kita Adventure', description: 'Kotlin, Android Studio, Figma', imageUrl: 'kita.png', url: "https://medium.com/@pablocigoy/case-study-abenteuer-kita-facilitating-communication-and-engagement-in-early-childhood-education-e14857209862" },
+        { title: 'PRM, UX/UI App design', description: 'UX and UI research with figma', imageUrl: 'prm_img.png', url: "https://medium.com/@pablocigoy/project-resources-management-ein-ux-ui-designprojekt-zur-f%C3%B6rderung-der-effektiven-zusammenarbeit-3a2b3dae2836" },
         { title: 'Android App  whatsApp', description: 'Kotlin, Android Studio, Figma', imageUrl: 'whatsApp.png', url: "https://github.com/Pablo-Mdz/WhatsSyntax" },
         { title: 'Be Chef - Web page', description: 'JS, React, MongoDB, Express, Tailwind', imageUrl: 'be-chef.png', url: "https://be-chef.netlify.app/", },
         { title: 'Sabor Latino - Web page', description: 'JS, Handlebars, Bootstrap, Express, MongoDB', imageUrl: 'sabor-latino.png', url: "https://thoughtful-cod-cloak.cyclic.app/", },
         { title: 'Padel Americano', description: 'JS, React, CSS', imageUrl: 'padel2.png', url: 'https://padel-americano.netlify.app' },
         { title: 'Save Oliver - Game', description: 'JS, P5', imageUrl: 'oliver.png', url: "https://pablo-mdz.github.io/Game-P5-Shooting/", },
-        // { title: 'Counter - Truco game', description: 'JS, CSS', imageUrl: 'truco.png', url: "https://dulcet-pudding-105e47.netlify.app/", },
+        { title: 'Counter - Truco game', description: 'JS, CSS', imageUrl: 'truco.png', url: "https://dulcet-pudding-105e47.netlify.app/", },
     ];
 
     return (
