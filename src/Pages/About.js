2310c374e285a06b168610e965f8663a3b9924a4
Pablo-Mdz <pablocigoy@gmail.com>
2024-07-16 14:19:43 +0200

    update about me

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -9,14 +9,13 @@ const About = () => {
         <div className="about-container">
             <h2 className="about-title">About Me</h2>
             <p className="about-text">
-                I am a software developer. Currently, I am studying mobile app development at the Syntaxt Institut, where I am learning Figma, Kotlin, and Swift.<br></br> In 2022, I completed the full stack web development bootcamp at Ironhack Berlin, and since then, I have been continuously learning and programming.<br></br>
+            I am a software developer specializing in mobile app development, with expertise in Figma, Kotlin, and Swift. I honed my mobile development skills at the Syntax Institut, where I learned to apply MVVM architecture, REST API, Firebase/Firestore, Moshi, Retrofit, Coroutines, and CoreData in my applications. <br></br>
 
-                Additionally, I work as a programming instructor at the ReDi bootcamp in Berlin. Outside of my professional life, my interests include cooking, traveling, and cycling.<br></br>  I have had the privilege of traveling to 38 countries and have worked in 5 of them, using my skills in teamwork, organizational tasks, task optimization, management, advice, and service in various environments, including private and public events. <br></br>
+I have also completed a full stack web development bootcamp at Ironhack Berlin in 2022, where I gained strong skills in JavaScript, React, NodeJS, and MongoDB. Since then, I have been continuously learning and programming. <br />
 
-                With over 10 years of experience working in kitchens, I am a sociable person who excels in team environments and is committed to constant improvement.
+Additionally, I work as a programming instructor at the ReDi bootcamp in Berlin, where I share my knowledge and passion for software development with students. Outside of my professional life, my interests include cooking, traveling, and cycling. <br />
 
-
-            </p>
+I have had the privilege of traveling to 38 countries and working in 5 of them, using my skills in teamwork, organizational tasks, task optimization, management, advice, and service in various environments, including private and public events. With over 10 years of experience working in kitchens, I am a sociable person who excels in team environments and is committed to constant improvement.   </p>
         </div>
     );
 };
